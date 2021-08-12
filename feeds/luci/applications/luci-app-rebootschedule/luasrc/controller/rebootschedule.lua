80543d6356c8abadb4136dc3378d22ec63d1941f
shidahuilang <jmkongdeshuai@gmail.com>
2021-08-12 11:29:37 +0800

    ·

--- feeds/luci/applications/luci-app-rebootschedule/luasrc/controller/rebootschedule.lua
+++
@@ -1,8 +0,0 @@
-module("luci.controller.rebootschedule", package.seeall)
-function index()
-if not nixio.fs.access("/etc/config/rebootschedule") then
-return
-end
-
-entry({"admin", "system", "rebootschedule"}, cbi("rebootschedule"), _("定时任务"),88)
-end
